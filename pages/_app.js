e9184bc86daf3ebb9543cc0d80613aaa5f2a8804
tin_pham <tindeptrai1@gmail.com>
2023-05-19 14:23:27 +0700

    fix runtime engine

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,8 @@ import Layout from '../components/layouts/main'
 import Fonts from '../components/fonts'
 import theme from '../lib/theme'
 import { AnimatePresence } from 'framer-motion'
-import Analytics from '@vercel/analytics'
+import { Analytics } from '@vercel/analytics/react'
+
 function Website({ Component, pageProps, router }) {
   return (
     <ChakraProvider theme={theme}>
