a759ebb5d239a33a256505088d6862b3997ba7aa
tin_pham <tindeptrai1@gmail.com>
2023-05-19 13:57:28 +0700

    fix build

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import Layout from '../components/layouts/main'
 import Fonts from '../components/fonts'
 import theme from '../lib/theme'
 import { AnimatePresence } from 'framer-motion'
-import { Analytics } from '@vercel/analytics'
+import Analytics from '@vercel/analytics'
 function Website({ Component, pageProps, router }) {
   return (
     <ChakraProvider theme={theme}>
